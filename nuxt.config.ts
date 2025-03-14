f9e41d3a730161b6c2523d318ac15b9545d0cfb9
dashixiong47 <475896756@qq.com>
2025-03-14 20:56:18 +0800

    修改构建文件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  nitro: {
+    preset: "cloudflare-pages",
+  },
   plugins: [
     '~/plugins/disable-warnings.js',
   ],
